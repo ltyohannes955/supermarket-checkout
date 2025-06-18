90cf9e2d5158aab4cb2a149b9817ce46e2489ede
ltyohannes955 <ltyohannes95@gmail.com>
2025-06-19 01:37:21 +0300

    added both routes for api use

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+import { Hono } from "hono";
+import { poweredBy } from "hono/powered-by";
+import { logger } from "hono/logger";
+import connectDB from "./db/connect";
+
+import productRoutes from "./routes/product.routes";
+import userRoutes from "./routes/user.routes";
+
+const app = new Hono();
+
+app.use("*", poweredBy());
+app.use("*", logger());
+
+// Mount routes
+app.route("/products", productRoutes);
+app.route("users", userRoutes);
+
+// Connect DB and start server
+(async () => {
+  await connectDB();
+
+  Bun.serve({
+    fetch: app.fetch,
+    port: 3000,
+  });
+})();
+
+console.log("🚀 Server running at http://localhost:3000");
