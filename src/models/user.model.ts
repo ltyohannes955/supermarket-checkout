44d22ba5b819463d0accf44ce314a8c55b1a1008
ltyohannes955 <ltyohannes95@gmail.com>
2025-06-19 15:03:47 +0300

    added time stamp and changed the export for order to access it

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -6,12 +6,13 @@ export interface IUserSchema {
   age: Date;
 }
 
-const UserSchema = new Schema<IUserSchema>({
-  name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  age: { type: Date, required: true },
-});
+const UserSchema = new Schema<IUserSchema>(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    age: { type: Date, required: true },
+  },
+  { timestamps: true }
+);
 
-const usersModel = model("users", UserSchema);
-
-export default usersModel;
+export default model("User", UserSchema);
