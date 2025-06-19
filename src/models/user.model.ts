ae1dea6cb94cf68839d82d37dcfb238c12136a98
ltyohannes955 <ltyohannes95@gmail.com>
2025-06-19 15:38:31 +0300

    changed age in to dateOfBirth

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -3,14 +3,14 @@ import { Schema, model } from "mongoose";
 export interface IUserSchema {
   name: string;
   email: string;
-  age: Date;
+  DateOfBirth: Date;
 }
 
 const UserSchema = new Schema<IUserSchema>(
   {
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
-    age: { type: Date, required: true },
+    DateOfBirth: { type: Date, required: true },
   },
   { timestamps: true }
 );
