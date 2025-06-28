eaddf52e14f7f932782df54d4c3e4fe3fb2140ae
ltyohannes955 <ltyohannes95@gmail.com>
2025-06-29 01:23:46 +0300

    added cors for frontend api requests

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { Hono } from "hono";
 import { poweredBy } from "hono/powered-by";
 import { logger } from "hono/logger";
 import connectDB from "./db/connect";
+import { cors } from "hono/cors";
 
 import productRoutes from "./routes/product.routes";
 import userRoutes from "./routes/user.routes";
@@ -10,6 +11,19 @@ import orderRoutes from "./routes/order.routes";
 
 const app = new Hono();
 
+app.use(
+  "*",
+  cors({
+    origin: (origin) => {
+      const allowed = [
+        "http://localhost:3000",
+        "https://supermarket-ui-seven.vercel.app/",
+      ];
+      return allowed.includes(origin ?? "") ? origin : "";
+    },
+    credentials: true,
+  })
+);
 app.use("*", poweredBy());
 app.use("*", logger());
 
